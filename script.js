565024305e3f04dac79c99d7ecf10f9e5f293894
NadirSarsri <nadir.sarsri@gmail.com>
2022-04-13 12:11:05 +0000

    removing some issues

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ const loadPosts = async () => {
       const img = await fetch("https://picsum.photos/1920");
       const blob = await img.blob();
       const imgURL = URL.createObjectURL(blob);
-
+      // J'ai pas aimé cette solution il y a une phase de non stabilité lors du chargeent
+      // utilises donc importNode()
       container.innerHTML += `
-        <div class='card col-sm-5 col-md-4 col-lg-3 px-0' onclick="showWindow('${imgURL}')">
+        <div class='card col-sm-6 col-md-4 col-lg-3 px-0' onclick="showWindow('${imgURL}')">
           <img src='${imgURL}' class='card-img-top'>
           <div class='card-body'>
             <h3 class='card-title mb-4'>${one.title}</h3>
-            <p class='card-description'>${one.body}</p>
+            <p class='card-text'>${one.body}</p>
           </div>
         </div>
       `;
@@ -25,14 +26,15 @@ const loadPosts = async () => {
 };
 
 loadPosts();
-document.getElementsByClassName('card');
+document.getElementsByClassName("card");
 
 // Show the window
-const showWindow = (img) => {
+const showWindow = (url) => {
   // html, body {margin: 0; height: 100%; overflow: hidden}
   // Add it when it shows up
+  console.log(url);
   const windowHTML = `<div class="window">
-  <img src="${img}" width="600">
+  <img src="${url}" width="600">
   <div class="close-btn" onclick='closeWindow()'>&times;</div>
 </div>`;
 
@@ -41,6 +43,6 @@ const showWindow = (img) => {
 };
 
 const closeWindow = () => {
-  document.getElementsByClassName("window")[0].style.display = "none";
+  // document.getElementsByClassName("window")[0].style.display = "none";
   document.getElementsByClassName("window")[0].remove();
 };
